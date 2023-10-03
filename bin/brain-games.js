deeb97ec051f1c51f381f92a2ba8064beeafcab1
Варвара <varvara@MacBook-Air-Varvara.local>
2023-10-03 16:00:18 +0700

    Функция спрашивания имени добавлена

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node.
+import { whatYouName } from "../src/cli.js";
+
+console.log("AAAAAAA");
 console.log("node bin/brain-games.js");
 console.log("Welcome to the Brain Games!");
+whatYouName();
