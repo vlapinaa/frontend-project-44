b237fe2a254c4d881e3dd46e1e59291c786db387
Варвара <varvara@MacBook-Air-Varvara.local>
2023-10-05 13:43:04 +0700

    Добавлена новая игра bin/brain-even.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node.
 import { whatYouName } from "../src/cli.js";
 
-console.log("AAAAAAA");
 console.log("node bin/brain-games.js");
 console.log("Welcome to the Brain Games!");
 whatYouName();
